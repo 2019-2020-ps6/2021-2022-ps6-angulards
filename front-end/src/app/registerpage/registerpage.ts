d7d138b6e56e690d80dedb6f63ddda7c40234c15
Aymaneelfiguigui <90783893+Aymaneelfiguigui@users.noreply.github.com>
2022-03-28 21:12:34 +0200

    Storing user info to the JSON server #8

--- front-end/src/app/registerpage/registerpage.ts
+++ front-end/src/app/registerpage/registerpage.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-registerpage',
@@ -7,10 +9,29 @@ import {Router} from '@angular/router';
   styleUrls: ['./registerpage.css']
 })
 export class RegisterComponent implements OnInit{
-  constructor(private router: Router) {
+  public signupForm !: FormGroup;
+  constructor(private router: Router, private formBuilder: FormBuilder, private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname: [''],
+      email: [''],
+      password: [''],
+      statcode: ['']
+    });
+  }
+  // tslint:disable-next-line:typedef
+  signUp(){
+    this.http.post<any>('http://localhost:8000/signUpUsers', this.signupForm.value)
+      .subscribe(res => {
+        alert('Reussie!');
+        this.signupForm.reset();
+        this.router.navigate(['login']);
+      }, err => {
+        alert('Failure');
+      });
+
   }
 
   redirectLogin(): void {

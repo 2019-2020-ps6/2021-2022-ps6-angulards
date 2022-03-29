7428615e9abd178ff04c22cf8959bcee430c4d1a
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-03-29 10:16:30 +0200

    show result draft

--- front-end/src/app/quiz-page/quiz-page.component.ts
+++ front-end/src/app/quiz-page/quiz-page.component.ts
@@ -4,12 +4,23 @@ import {ActivatedRoute} from '@angular/router';
 import {Quiz} from '../../models/quiz.model';
 import {QuizService} from '../../services/quiz.service';
 
+
 @Component({
   selector: 'app-quiz-page',
   templateUrl: './quiz-page.component.html',
   styleUrls: ['./quiz-page.component.scss']
 })
 export class QuizPageComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, public quizService: QuizService) {
+    this.quizService.quizSelected$.subscribe((quiz: Quiz) => {
+      this.quiz = quiz;
+    });
+  }
+
+  public DISPLAY_RIGHT_ANSWER = 2;
+  public DISPLAY_WRONG_ANSWER = 1;
+  public DISPLAY_NO_ANSWER = 0;
 
   quiz: Quiz;
   question: Question;
@@ -18,34 +29,38 @@ export class QuizPageComponent implements OnInit {
   indexQuiz = 0;
   selectedAnswer = new Map();
   id: string;
-  resultAffiche = false;
-
-  constructor(private route: ActivatedRoute, public quizService: QuizService) {
-    this.quizService.quizSelected$.subscribe((quiz: Quiz) => {
-      this.quiz = quiz;
-    });
-  }
+  displayResult = this.DISPLAY_NO_ANSWER;
 
   ngOnInit(): void {
-    console.log(this.question);
     this.id = this.route.snapshot.paramMap.get('id');
     this.quizService.setSelectedQuiz(this.id);
   }
 
+  getQuestionsLength(): number {
+    return this.quiz.questions.length - 1;
+  }
+
+  getAnswersLength(): number {
+    return this.quiz.questions[this.indexQuiz].answers.length;
+  }
+
   getCorrectAnswer(): Answer {
-    for (let i = 0; i < this.question.answers.length; i++) {
+    const nbAns = this.getAnswersLength();
+    for (let i = 0; i < nbAns; i++) {
       if (this.quiz.questions[this.indexQuiz].answers[i].isCorrect) {
         return this.quiz.questions[this.indexQuiz].answers[i];
       }
     }
   }
 
-  incrementCorrect(answer): void {
+  isAnswerCorrect(answer): void {
     const correct = this.getCorrectAnswer().value;
     if (correct === answer) {
       this.correctAnswer++;
+      this.displayResult = this.DISPLAY_RIGHT_ANSWER;
+    } else {
+      this.displayResult = this.DISPLAY_WRONG_ANSWER;
     }
-    this.resultAffiche = true;
     this.selectedAnswer.set(this.indexQuiz, answer);
   }
 
@@ -54,6 +69,6 @@ export class QuizPageComponent implements OnInit {
   }
 
   isEnd(): boolean {
-    return this.indexQuiz >= this.quiz.questions.length - 1;
+    return this.indexQuiz >= this.getQuestionsLength();
   }
 }

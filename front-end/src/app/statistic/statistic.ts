9514a5eef4fedf77ff1e2dc351f046184959b7ae
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-13 14:49:12 +0200

    [DEV] statistic : getting wrong answers statistic for a specific user #15

--- front-end/src/app/statistic/statistic.ts
+++ front-end/src/app/statistic/statistic.ts
@@ -14,12 +14,9 @@ import {Response} from '../../models/response.model';
 })
 // tslint:disable-next-line:component-class-suffix
 export class Statistic implements OnInit {
-  statistic: number;
-  userId: string;
-  questionId: string;
-  quizId: string;
 
-  question: Question;
+  userId: string;
+
   response: Response;
 
 
@@ -32,35 +29,10 @@ export class Statistic implements OnInit {
   }
 
   getNumberWrongAnswer(): void {
-
-    /*fetch('http://localhost:9428/api/users/response')
-      .then(res => res.json())
-      .then(data => console.log(data.userId));*/
-
-    // change the request demande from users to quizzes
-   /* this.http.get<any>('http://localhost:9428/api/users/response')
-      .subscribe(res => {
-        const stat = res.find((a: any) => {
-
-            if (this.userId === a.userId) {
-              return a.wrongAnswerCount;
-            }
-
-          },
-          err => {
-            alert('Error');
-          });
-        console.log('stat ', stat, this.statistic);
-        this.statistic = stat.wrongAnswerCount;
-        this.questionId = stat.questionId;
-        this.quizId = stat.quizId;
-      });*/
-
     /*
-    console.log(' qst id and quiz id : ', this.questionId, this.quizId);
-
     this.question = this.http.get<Question>('http://localhost:9428/api/quizzes/' + this.quizId + '/questions/' + this.questionId)
-      .subscribe(res => {});*/
+      .subscribe(res => {});
+      */
 
     this.http.get<Response>('http://localhost:9428/api/users/response')
       .subscribe(res => {

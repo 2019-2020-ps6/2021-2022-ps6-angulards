6813701d99d29e104badc3774e6fbc9e90824c8f
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-05-01 14:35:52 +0200

    [DEV] mock for user #28

---
+++ front-end/src/mocks/user.mock.ts
@@ -0,0 +1,14 @@
+import {User} from '../models/user.model';
+
+export const UserMock: { firstName: string; lastName: string; password: string; admin: boolean; id: string; email: string }[] = [
+  {
+    
+    id: 'userMock',
+    email: 'emailMock',
+    firstName: 'UserFirstName',
+    lastName: 'UserLastName',
+    password: 'mdp',
+    admin: false,
+
+  }
+  ];
